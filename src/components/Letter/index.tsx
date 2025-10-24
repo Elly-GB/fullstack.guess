a8fea9d04a597037c488cec8520f1b252f9d8bfb
Gaby <gabyfranbublitz@gmail.com>
2025-10-24 10:30:01 -0300

    added styles import

--- src/components/Letter/index.tsx
+++ src/components/Letter/index.tsx
@@ -1,10 +1,12 @@
+import styles from "./styles.module.css"
+
 type Props = {
   value?: string
 }
 
 export function Letter({ value = "" }: Props) {
   return (
-    <div>
+    <div className={styles.letter}>
       <span>{value}</span>
     </div>
   )
