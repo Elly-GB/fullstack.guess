37ab320a96efa71fde54a6083b8d71111648522d
Gaby <gabyfranbublitz@gmail.com>
2025-10-26 20:25:25 -0300

    added reset tip

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import { LettersUsed } from "./components/LettersUsed"
 import type { LettersUsedProps } from "./components/LettersUsed"
 
 export default function App() {
+  const [score, setScore] = useState(0)
   const [letter, setLetter] = useState("")
   const [attempts, setAttempts] = useState(0)
   const [lettersUsed, setLettersUsed] = useState<LettersUsedProps[]>([])
@@ -48,8 +49,13 @@ export default function App() {
       return alert("Você já utilizou a letra " + value)
     }
 
-    setLettersUsed((prevState) => [...prevState, { value, correct: false }])
+    const hits = challenge.word.toUpperCase().split("").filter((char) => char === value).length
 
+    const correct = hits > 0
+    const currentScore = score + hits
+
+    setLettersUsed((prevState) => [...prevState, { value, correct }])
+    setScore(currentScore)
     setLetter("")
   }
 
@@ -66,7 +72,7 @@ export default function App() {
     <div className={styles.container}>
       <main>
         <Header current={attempts} max={10} onRestart={handleRestartGame} />
-        <Tip tip="Uma das linguagens de programação mais utilizadas" />
+        <Tip tip={challenge.tip} />
 
         <div className={styles.word}>
           {challenge.word.split("").map(() => (
